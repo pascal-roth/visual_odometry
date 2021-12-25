2d69a01b62af7abd2712b354318c0d67d2589847
Max Fruehauf <max@maxfruehauf.com>
2021-12-25 18:21:30 +0100

    Add pointcloud visualization placeholder

--- vio_pipeline/viewer.py
+++ vio_pipeline/viewer.py
@@ -2,9 +2,11 @@ import threading
 import numpy as np
 from vispy import scene, app, gloo
 from vispy.visuals import ImageVisual
+from vispy.scene import visuals
 
 from queue import Queue
 from threading import Thread
+from params import * 
 
 
 class Viewer(object):
@@ -31,8 +33,10 @@ class Viewer(object):
 
     def start_vis(self):
         # create canvas to draw on, Window size is fixed to (1280, 720)
-        canvas = scene.SceneCanvas(keys="interactive", size=(1280, 720), show=True)
-        canvas.title="Scaramuzza Hire Me!!"
+        canvas = scene.SceneCanvas(keys="interactive",
+                                   size=(1280, 720),
+                                   show=True)
+        canvas.title = "Scaramuzza Hire Me!!"
         self.showing_plot = True
 
         # Attach key handler to close window
@@ -46,32 +50,49 @@ class Viewer(object):
         # which are automaticall resized with the grid
         grid = canvas.central_widget.add_grid()
 
-
         # Add trajectory view
         trajectory_view = grid.add_view(row=0, col=0)
+        point_cloud_scatter = visuals.Markers()
+        trajectory_view.add(point_cloud_scatter)
+        # Add XYZ Axis at the origin as a reference
+        axis = visuals.XYZAxis(parent=trajectory_view.scene)
 
         # Add image view
         img_view = grid.add_view(row=0, col=1)
-        interpolation = "nearest"
-
         img_view.camera = scene.PanZoomCamera(aspect=1)
         img_view.camera.flip = (0, 1, 0)
-
+        # since we don't have the image data at this point,
+        # leave data none and crate the image AFTER the view,
+        # otherwise vispy does not allow updates on the image data
         img_vispy: ImageVisual = scene.visuals.Image(None,
-                                        interpolation="nearest",
-                                        parent=img_view.scene,
-                                        method="subdivide")
-        trajectory = []
-        img_data = None
+                                                     interpolation="nearest",
+                                                     parent=img_view.scene,
+                                                     method="subdivide")
+
+        # Sample visualization of some points
+        # Gets removed as soon as a pose comes in
+        # TODO: remove this as soon as poses sent to the pose_queue
+        trajectory = np.random.normal(size=(1 << 10, 3))
+        point_cloud_scatter.set_data(np.array(trajectory),
+                                     edge_color=None,
+                                     face_color=(1, 1, 1, .5),
+                                     size=5)
+        trajectory_view.camera = "turntable"
 
         def update(ev):
             if not self.showing_plot:
                 self.timer.stop()
+
             # get newest pose if available
             if not self.pose_queue.empty():
                 while not self.pose_queue.empty():
                     pose = self.pose_queue.get()
                 trajectory.append(pose[:3, 3])
+                point_cloud_scatter.set_data(np.array(trajectory),
+                                             edge_color=None,
+                                             face_color=(1, 1, 1, .5),
+                                             size=5)
+                trajectory_view.camera = "turntable"
 
             # get newest image if available
             if not self.image_queue.empty():
@@ -80,8 +101,6 @@ class Viewer(object):
                 img_vispy.set_data(img_data)
                 img_view.camera.set_range()
 
-
+        # start the timer & update at the desired framerate
         self.timer.connect(update)
-        self.timer.start(0)
-
-
+        self.timer.start(1/TARGET_FRAMERATE)
